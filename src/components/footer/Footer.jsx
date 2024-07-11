cc7430aa4ec6f727269fd8c60f5fd9d6520dd3af
leewebstechnologies <aliuajibade317@gmail.com>
2024-07-11 10:47:14 +0100

    add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,138 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div
+        className="container-fluid footer mt-5 py-5 wow fadeIn"
+        data-wow-delay="0.1s"
+      >
+        <div className="container py-5">
+          <div className="row g-5">
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-4">Our Office</h4>
+              <p className="mb-2">
+                <i className="fa fa-map-marker-alt me-3" />
+                123 Street, New York, USA
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-phone-alt me-3" />
+                +012 345 67890
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-envelope me-3" />
+                info@example.com
+              </p>
+              <div className="d-flex pt-3">
+                <a
+                  className="btn btn-square btn-light rounded-circle me-2"
+                  href
+                >
+                  <i className="fab fa-twitter" />
+                </a>
+                <a
+                  className="btn btn-square btn-light rounded-circle me-2"
+                  href
+                >
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a
+                  className="btn btn-square btn-light rounded-circle me-2"
+                  href
+                >
+                  <i className="fab fa-youtube" />
+                </a>
+                <a
+                  className="btn btn-square btn-light rounded-circle me-2"
+                  href
+                >
+                  <i className="fab fa-linkedin-in" />
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <a className="btn btn-link" href>
+                Home
+              </a>
+              <a className="btn btn-link" href>
+                Start A Blog
+              </a>
+              <a className="btn btn-link" href>
+                Start A Youtube Channel
+              </a>
+              <a className="btn btn-link" href>
+                AI Income Blueprint (New)
+              </a>
+              <a className="btn btn-link" href>
+                Income University
+              </a>
+              <a className="btn btn-link" href>
+                My Story
+              </a>
+              <a className="btn btn-link" href>
+                Contact
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-4">Business Hours</h4>
+              <p className="mb-1">Monday - Friday</p>
+              <p className="mb-1">09:00 am - 07:00 pm</p>
+              <br />
+
+              <p className="mb-1">Saturday</p>
+              <p className="mb-1">09:00 am - 07:00 pm</p>
+              <br />
+              <p className="mb-1">Sunday</p>
+              <p className="mb-1">Closed</p>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-4">Newsletter</h4>
+              <p>Sign Up For the FREE 0-$10k Online Business Masterclass</p>
+              <div className="position-relative w-100">
+                <input
+                  className="form-control bg-transparent w-100 py-3 ps-4 pe-5"
+                  type="text"
+                  placeholder="Your email"
+                />
+                <button
+                  type="button"
+                  className="btn btn-light py-2 position-absolute top-0 end-0 mt-2 me-2"
+                >
+                  SignUp
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Footer End */}
+      {/* Copyright Start */}
+      <div className="container-fluid copyright py-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+              ©
+              <a className="fw-medium text-light" href="#">
+                Isiak Olaniyan
+              </a>
+              , All Right Reserved.
+            </div>
+            <div className="col-md-6 text-center text-md-end">
+              Designed By Isiak Olaniyan
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Copyright End */}
+      {/* Back to Top */}
+      <a
+        href="#"
+        className="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"
+      >
+        <i className="bi bi-arrow-up" />
+      </a>
+    </>
+  );
+};
+export default Footer;
--- src/components/sumup/Sumup.jsx
+++ src/components/sumup/Sumup.jsx
@@ -1,4 +1,7 @@
 import "./sum.css";
+import one from "../../images/laptop.png";
+import two from "../../images/economic.png";
+import three from "../../images/equity.png";
 
 const Sumup = () => {
   return (
@@ -13,49 +16,80 @@ const Sumup = () => {
           If you want to learn how to start a blog to make life-changing money,
           you need to stop writing and start developing systems to scale your
           blog like a business, not a hobby.
-        </p>
-        <p>
+          <br />
           I launched this blog in January 2019 and as you can see from my Blog
-          Income Reports, I now make over $250k/month.
-        </p>
-        <p>
+          Income Reports, I now make over $250k/month. <br />
           Two years ago, I was at a corporate job, building my blog on the side.
-        </p>
-        <p>
+          <br />
           I took what I learned in the startup world and started to scale my
-          personal brand like a startup.
-        </p>
-        <p>
+          personal brand like a startup. <br />
           By treating my blog like a real business from day one, I stopped being
-          the tortured writer and became the CEO.
-        </p>
-        <p>
+          the tortured writer and became the CEO. <br />
           <b>First, let’s face it – most blogging advice is outdated.</b>
-        </p>
-        <p>
+          <br />
           You’re sold the dream of "passive income" (which doesn't exist), all
           while 99% of bloggers are still failing, and the same old school
           “influencers” who started 5-10 years ago are the ones making money
-          online.
-        </p>
-        <p>I hate to break it to you, but you’ve been lied to.</p>
-        <p>
+          online. <br />
+          I hate to break it to you, but you’ve been lied to. <br />
           These old-school influencers tell you to write about your passions,
           stay consistent, form good writing habits, and create fantastic
           content. Then, a few years from now, you’ll finally have enough
           traffic to monetize your blog with affiliate links, ads, online
-          courses, or other digital products.
-        </p>
-        <p>To grow your blog in the 2020s, you need new tactics. </p>
-        <p>
+          courses, or other digital products. <br />
+          To grow your blog in the 2020s, you need new tactics. <br />
           <b>
             The entire reason I created this blog was to abandon this old advice
             and update your blogging tactics.
           </b>
-        </p>
-        <p>
+          <br />
           <b>Our community is built to help you:</b>
+          <br />
         </p>
+        <div className="sumup_cards">
+          <div className="card">
+            <img className="laptop" src={one} alt="laptop" />
+            <h6>Step 1: Launch Your Blog</h6>
+            <p>
+              I’ve launched hundreds of websites and will help you discover your
+              unique niche and value proposition, expertly plan your content
+              strategy, choose the best web hosting, email marketing software,
+              and even podcast hosting to start your online business.
+            </p>
+          </div>
+          <div className="card">
+            <img className="growth" src={two} alt="growth" />
+            <h6>Step 2: Scale Your Growth</h6>
+            <p>
+              I’ve launched hundreds of websites and will help you discover your
+              unique niche and value proposition, expertly plan your content
+              strategy, choose the best web hosting, email marketing software,
+              and even podcast hosting to start your online business.
+            </p>
+          </div>
+          <div className="card">
+            <img className="equity" src={three} alt="equity" />
+            <h6>Step 3: Monetize Faster</h6>
+            <p>
+              I’ve launched hundreds of websites and will help you discover your
+              unique niche and value proposition, expertly plan your content
+              strategy, choose the best web hosting, email marketing software,
+              and even podcast hosting to start your online business.
+            </p>
+          </div>
+        </div>
+        <p className="final">
+          Finally, I want to stop and say I’m glad you’re here. I deeply value
+          every single person I’ve met through this blog. <br />
+          <b>And above all, it’s my personal mission to help you succeed.</b>
+          <br /> As long as you’re willing to put in the effort, I’ll do
+          whatever it takes to help get you there. <br />
+        </p>
+      </div>
+      <div className="signup">
+        <h3>Sign Up For the FREE 0-$10k Online Business Masterclass</h3>
+        <button className="masterclass_btn">Watch the FREE Masterclass</button>
+        <h5>It's FREE and your information is 100% secure. 🔒 </h5>
       </div>
     </>
   );
