26b4e53274f19e0c8b09f792a3979966f8178552
leewebstechnologies <aliuajibade317@gmail.com>
2024-07-13 00:00:19 +0100

    add blog page

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,10 +1,21 @@
 import "./blog.css";
+import bloggers from "../../images/bloggers.jpg";
 
 const Blog = () => {
   return (
     <>
-      <h1>Blog</h1>
-      
+      <div className="blog">
+        <img className="blogger" src={bloggers} alt="blogger" />
+        <h2>How to Build a Blogging Business</h2>
+        <p>
+          I launched my blog in 2019 and scaled it to a 7-figure business in
+          under 2 years. I want to share everything I learned along the way.
+          <b> Watch my blogging videos</b> on YouTube.
+        </p>
+        <button className="masterclass_btn">
+          Get the Free 0-$10k Business Masterclass
+        </button>
+      </div>
     </>
   );
 };
